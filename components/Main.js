42b79e9cb6fd62795182c8ddaed3fa0db3e94e78
gp-kakimoto <kakimoto_t@gp1000.jp>
2024-06-12 14:11:50 +0900

    スタイルファイルの分割

--- components/Main.js
+++ components/Main.js
@@ -1,13 +1,13 @@
-import styles from '../styles/Home.module.css'
+import classes from './Main.module.css'
 import { Links } from '../components/links'
 import { Headline } from '../components/Headline'
 export function Main(props) {
   return (
-      <main className={styles.main}>
+      <main className={classes.main}>
         <Headline 
             page={props.page}
         >
-          <code className={styles.code}>page/{props.page}.js</code>
+          <code className={classes.code}>page/{props.page}.js</code>
         </Headline>
         <Links />
       </main>
