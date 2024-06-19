93b83b31762224789a4054e675dcd33cc0d1198a
gp-kakimoto <kakimoto_t@gp1000.jp>
2024-06-19 15:35:32 +0900

    useStateを用いたカウントアップ処理

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import styles from 'src/styles/Home.module.css'
 import {Footer} from 'src/components/Footer'
 import {Main} from 'src/components/Main'
 import { Header } from 'src/components/Header'
-import { useCallback,useEffect } from 'react'
+import { useCallback,useEffect, useState } from 'react'
 
 //const handleClick = (e, foo) => {
 //  console,log(e.target.href);
@@ -13,19 +13,16 @@ import { useCallback,useEffect } from 'react'
 
 export default function Home() {
 
-  const foo = 1;
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
-  },[]);
+  const [count,setCount] = useState(1);
 
+  const handleClick = (e) => {
+    setCount((count) => count+1 );
+    setCount((count) => count+1 );
+  }
 
   useEffect(() => {
-    console.log("マウント時");
     document.body.style.backgroundColor = "lightblue";
     return () => {
-      console.log("アンマウント時");
       document.body.style.backgroundColor = "";
     };
   },[]);
@@ -36,15 +33,8 @@ export default function Home() {
         <title>Index Next App</title>
       </Head>
       <Header />
-      <a href="/about" onClick={handleClick}>
-        ボタン
-      </a>
- 
-    {/*  <a href ="/about" onClick={(e) => {
-        handleClick(e,foo)
-      }}>
-        ボタン
-      </a> */}
+      <h1>{count}</h1>
+      <button onClick={handleClick}>ボタン</button>
     
       <Main page="index" />
       <Footer />
