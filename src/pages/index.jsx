eb8917f81aa6fa2061e876e21fafec521c1f7e10
gp-kakimoto <kakimoto_t@gp1000.jp>
2024-06-21 15:57:38 +0900

    useStateとuseCallbackの学習のためのコミット

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,29 +1,50 @@
-import Head from 'next/head'
-import styles from 'src/styles/Home.module.css'
-import {Footer} from 'src/components/Footer'
-import {Main} from 'src/components/Main'
-import { Header } from 'src/components/Header'
-import { useCallback,useEffect, useState } from 'react'
+import Head from "next/head";
+import styles from "src/styles/Home.module.css";
+import { Footer } from "src/components/Footer";
+import { Main } from "src/components/Main";
+import { Header } from "src/components/Header";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
-
-  const [count,setCount] = useState(1);
-
-  const handleClick = useCallback (
-    () => {
-      if (count < 10) {
-        setCount((count) => count+1 );
-      }
-    },
-    [count]
-  ); // countが変化するたびにメソッドが再生成される
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
+  const handleClick = useCallback(() => {
+    if (count < 10) {
+      setCount((count) => count + 1);
+    }
+  }, [count]); // countが変化するたびにメソッドが再生成される
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
     return () => {
       document.body.style.backgroundColor = "";
     };
-  },[]);
+  }, []);
+
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert("5文字以内にしてください");
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
+
+  //const handleDisplay = useCallback(() => {
+  //  setIsShow((isShow) => !isShow);
+  // }, []);
+
+  //const hoge = () => {
+  //  setIsShow((isShow) => !isShow);
+  // };
+
+  const invertTrueOrFalse = (isShow) => !isShow;
+
+  const setTrueOrFalse = () => {
+    setIsShow(invertTrueOrFalse);
+  };
+
+  const handleDisplay = useCallback(setTrueOrFalse, []);
 
   return (
     <div className={styles.container}>
@@ -31,10 +52,12 @@ export default function Home() {
         <title>Index Next App</title>
       </Head>
       <Header />
-      <h1>{count}</h1>
-      <button onClick={handleClick}>ボタン</button>    
+      {isShow ? <h1>{count}</h1> : null}
+      <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      <input type="text" value={text} onChange={handleChange} />
       <Main page="index" />
       <Footer />
     </div>
-  )
+  );
 }
