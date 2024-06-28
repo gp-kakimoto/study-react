eedf5d67461a8bdb622eeae80f5e6c9fbf567133
gp-kakimoto <kakimoto_t@gp1000.jp>
2024-06-28 16:04:14 +0900

    ページ間のstateのリフトアップについて

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,12 +1,20 @@
-import Head from 'next/head'
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
+import { useCounter } from "src/hooks/useCounter";
+import { useInputArray } from "src/hooks/useInputArray";
+import { useBgLightBlue } from "src/hooks/useBgLightBlue";
 
 function MyApp({ Component, pageProps }) {
+  //const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const counter = useCounter();
+  //const { text, array, handleChange, handleAdd } = useInputArray();
+  const inputArray = useInputArray();
+  useBgLightBlue();
 
-<Head>
-  <link rel="icon" href="/favicon.ico" />
-</Head>
-  return <Component {...pageProps} />
+  <Head>
+    <link rel="icon" href="/favicon.ico" />
+  </Head>;
+  return <Component {...pageProps} {...counter} {...inputArray} />;
 }
 
-export default MyApp
+export default MyApp;
