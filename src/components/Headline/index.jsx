2cba86c1c75a7b95abc068be1df38edf32ac3e1a
gp-kakimoto <kakimoto_t@gp1000.jp>
2024-07-03 14:34:29 +0900

    コンポーネント間のstateのリフトアップについて

--- src/components/Headline/index.jsx
+++ src/components/Headline/index.jsx
@@ -1,17 +1,15 @@
 //import React from "react";
-import classes from 'src/components/Headline/Headline.module.css'
+import { checkIsOnDemandRevalidate } from "next/dist/server/api-utils";
+import classes from "src/components/Headline/Headline.module.css";
 export function Headline(props) {
-
   return (
-        <div>
-            <h1 className={classes.title}>
-                {props.page} page
-            </h1>
+    <div>
+      <h1 className={classes.title}>{props.page} page</h1>
 
-            <p className={classes.description}>
-                Get started by editing {props.children}
-            </p>
-
-        </div>
-  )
+      <p className={classes.description}>
+        アイテムの数は {props.children} 個です
+      </p>
+      <button onClick={props.handleReduce}>減らす</button>
+    </div>
+  );
 }
